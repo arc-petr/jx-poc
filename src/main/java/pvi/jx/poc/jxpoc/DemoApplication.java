83f42f28307c01e3e79e1515262c4eeaa9a5db30
Petr Igrevskiy <pigrevskiy@reputation.com>
2019-07-24 15:12:06 -0700

    Initial import

---
+++ src/main/java/pvi/jx/poc/jxpoc/DemoApplication.java
@@ -0,0 +1,13 @@
+package pvi.jx.poc.jxpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
